21226bd736e2447f45a6bfabaa10cbd2c1c41133
Om-Gor <202101484@daiict.ac.in>
2023-08-07 06:29:11 +0530

    gfg potd

---
+++ BST.cpp
@@ -0,0 +1,100 @@
+
+/*
+
+
+░██████╗░  ░█████╗░  ██████╗░
+██╔════╝░  ██╔══██╗  ██╔══██╗
+██║░░██╗░  ██║░░██║  ██████╔╝
+██║░░╚██╗  ██║░░██║  ██╔══██╗
+╚██████╔╝  ╚█████╔╝  ██║░░██║
+░╚═════╝░  ░╚════╝░  ╚═╝░░╚═╝
+
+
+░█████╗░███╗░░░███╗
+██╔══██╗████╗░████║
+██║░░██║██╔████╔██║
+██║░░██║██║╚██╔╝██║
+╚█████╔╝██║░╚═╝░██║
+░╚════╝░╚═╝░░░░░╚═╝
+
+
+
+
+                   YAao,
+                    Y8888b,
+                  ,oA8888888b,
+            ,aaad8888888888888888bo,
+         ,d888888888888888888888888888b,
+       ,888888888888888888888888888888888b,
+      d8888888888888888888888888888888888888,
+     d888888888888888888888888888888888888888b
+    d888888P'                    `Y888888888888,
+    88888P'                    Ybaaaa8888888888l
+   a8888'                      `Y8888P' `V888888
+ d8888888a                                `Y8888
+AY/'' `\Y8b                                 ``Y8b
+Y'      `YP                                    ~~
+         `'
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define ha cout<<"YES"<<endl
+#define na cout<<"NO"<<endl
+#define fr(x,n) for(int i=x;i<n;i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+const long long mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void solve(Node*root, vector<int>&a){
+    if(root==NULL)return;
+
+    solve(root->left,a);
+    a.push_back(root->data);
+    solve(root->right,a);
+    
+}
+
+
+float findMedian(struct Node *root)
+{
+    if(root==NULL)return 0;
+    if(root->left==NULL && root->right==NULL)return root->data;
+    vector<int>n;
+    solve(root,n);
+    int k = n.size();
+    if(k%2!=0)return 1.00*n[k/2];
+    else{
+        return 1.00 * (n[k / 2] + n[k / 2 - 1]) / 2.0;
+    }
+    
+}
+
+
+
+int main(){
+
+    
+
+
+
+    return 0;
+}
---
+++ kth ansister.cpp
@@ -0,0 +1,92 @@
+
+/*
+
+
+░██████╗░  ░█████╗░  ██████╗░
+██╔════╝░  ██╔══██╗  ██╔══██╗
+██║░░██╗░  ██║░░██║  ██████╔╝
+██║░░╚██╗  ██║░░██║  ██╔══██╗
+╚██████╔╝  ╚█████╔╝  ██║░░██║
+░╚═════╝░  ░╚════╝░  ╚═╝░░╚═╝
+
+
+░█████╗░███╗░░░███╗
+██╔══██╗████╗░████║
+██║░░██║██╔████╔██║
+██║░░██║██║╚██╔╝██║
+╚█████╔╝██║░╚═╝░██║
+░╚════╝░╚═╝░░░░░╚═╝
+
+
+
+
+                   YAao,
+                    Y8888b,
+                  ,oA8888888b,
+            ,aaad8888888888888888bo,
+         ,d888888888888888888888888888b,
+       ,888888888888888888888888888888888b,
+      d8888888888888888888888888888888888888,
+     d888888888888888888888888888888888888888b
+    d888888P'                    `Y888888888888,
+    88888P'                    Ybaaaa8888888888l
+   a8888'                      `Y8888P' `V888888
+ d8888888a                                `Y8888
+AY/'' `\Y8b                                 ``Y8b
+Y'      `YP                                    ~~
+         `'
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define ha cout<<"YES"<<endl
+#define na cout<<"NO"<<endl
+#define fr(x,n) for(int i=x;i<n;i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+const long long mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+struct Node
+{
+	int data;
+	struct Node *left, *right;
+};
+
+void makepar(Node* root, map<Node*,Node*>&mp){
+    if(root==NULL)return;
+    if(root->left){
+        mp[root->left]= root;
+    }
+    if(root->right){
+        mp[root->right]=root;
+    }
+    root=root->left;
+    
+
+}
+
+int kthAncestor(Node *root, int k, int node)
+{
+    map<Node*,Node*>mp;
+    makepar(root, mp);
+
+    
+}
+
+
+
+
+
+int main(){
+
+    
+
+
+
+    return 0;
+}
