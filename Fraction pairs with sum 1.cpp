1abec9717640141e238a411258bba3eb2467cc56
Om-Gor <202101484@daiict.ac.in>
2023-08-08 10:06:18 +0530

    08-08-2023 potd question

---
+++ Fraction pairs with sum 1.cpp
@@ -0,0 +1,98 @@
+
+/*
+
+
+░██████╗░  ░█████╗░  ██████╗░
+██╔════╝░  ██╔══██╗  ██╔══██╗
+██║░░██╗░  ██║░░██║  ██████╔╝
+██║░░╚██╗  ██║░░██║  ██╔══██╗
+╚██████╔╝  ╚█████╔╝  ██║░░██║
+░╚═════╝░  ░╚════╝░  ╚═╝░░╚═╝
+
+
+░█████╗░███╗░░░███╗
+██╔══██╗████╗░████║
+██║░░██║██╔████╔██║
+██║░░██║██║╚██╔╝██║
+╚█████╔╝██║░╚═╝░██║
+░╚════╝░╚═╝░░░░░╚═╝
+
+
+
+
+                   YAao,
+                    Y8888b,
+                  ,oA8888888b,
+            ,aaad8888888888888888bo,
+         ,d888888888888888888888888888b,
+       ,888888888888888888888888888888888b,
+      d8888888888888888888888888888888888888,
+     d888888888888888888888888888888888888888b
+    d888888P'                    `Y888888888888,
+    88888P'                    Ybaaaa8888888888l
+   a8888'                      `Y8888P' `V888888
+ d8888888a                                `Y8888
+AY/'' `\Y8b                                 ``Y8b
+Y'      `YP                                    ~~
+         `'
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define ha cout<<"YES"<<endl
+#define na cout<<"NO"<<endl
+#define fr(x,n) for(int i=x;i<n;i++)
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+const long long mod = 1000000007;
+const long double pi=3.14159265358979323846264338327950288419716939937510582097494459230;
+
+
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    int countFractions(int n, int num[], int deno[])
+    {
+        int cnt=0;
+        map<double,int> mp;
+        for(int i=0; i<n; i++){
+            double t1= (double)(num[i])/deno[i];
+            double t2= (double)(deno[i]-num[i])/deno[i];
+            cnt+= mp[t2];
+            mp[t1]++;
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int numerator[n],denominator[n];
+        for(int i=0;i<n;i++)
+            cin>>numerator[i];
+        for(int i=0;i<n;i++)
+            cin>>denominator[i];
+        Solution ob;
+        int ans=ob.countFractions(n,numerator,denominator);
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
